f069d531aaf4eed7ca521cac6d055ed3ea77caca
SJoJoK <450986321@qq.com>
2021-06-26 01:29:33 +0800

    地图已经导入了，准备睡觉

--- client/src/lib/echarts.js
+++ client/src/lib/echarts.js
@@ -3,7 +3,7 @@ import echarts from 'echarts/lib/echarts'
 // 引入提示框和标题组件
 import 'echarts/lib/component/legend'
 import 'echarts/lib/component/title'
-
+import 'echarts/extension/bmap/bmap' //引入百度地图
 import 'echarts/lib/chart/bar'     // 引入柱状图
 import 'echarts/lib/chart/pie'   // 引入饼状图
 import 'echarts/lib/chart/line'   // 引入折线图

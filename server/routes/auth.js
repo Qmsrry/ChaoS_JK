04a59e315a6867ace56bc62179f9a27bb566c9ed
SJoJoK <450986321@qq.com>
2021-06-27 02:50:43 +0800

    邮件发送成功！明天能写报告了

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Auth = mongoose.model('Auth');
 
-const smtp = require('./config/smtp.js')();
+const transport = require('../config/smtp.js')();
 /* Get username & passwod, Return the token */
 router.post('/', function (req, res, next) {
   const{username, password} = req.body
@@ -51,6 +51,20 @@ router.put('/code', async function (req, res, next) {
     res.status(400);
     res.send({ message: '已有相同邮箱' });
   }
+  transport.sendMail({
+    from: 'csjk@zju.edu.cn', // 发件邮箱
+    to: email, // 收件列表
+    subject: '验证你的电子邮件', // 标题
+    html: `
+     <p>你好！</p>
+     <p>您正在注册ChaoS_JK账号</p>
+     <p>你的验证码是：<strong style="color: #ff4e2a;">123456</strong></p>
+     <p>***该验证码5分钟内有效***</p>` // html 内容
+  },
+    function (error, data) {
+      if (error) console.log(error);
+      transport.close(); // 如果没用，关闭连接池
+    })
   res.status(201);
   res.send();
 });

c700216785f48266ecc66a94c16e8b6a51906ea2
SJoJoK <450986321@qq.com>
2021-06-27 18:41:08 +0800

    bug fixed

--- mqtt/config/mongoose.js
+++ mqtt/config/mongoose.js
@@ -3,7 +3,7 @@ const {mongodb} = require('../../.config.js');
 
 module.exports = function () {
     mongoose.set('useCreateIndex', true)
-    const db = mongoose.connect('mongodb://'+mongodb.db+':'+mongodb.port+'/'+mongodb.db, { useNewUrlParser: true, useUnifiedTopology: true, keepAlive: 120 });
+    const db = mongoose.connect('mongodb://'+mongodb.ip+':'+mongodb.port+'/'+mongodb.db, { useNewUrlParser: true, useUnifiedTopology: true, keepAlive: 120 });
     require('../../db/models/auth');
     require('../../db/models/user');
     require('../../db/models/device')
